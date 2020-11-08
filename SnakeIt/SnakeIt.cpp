66f0754251c33c6171c29a29630c7933a743f744
Twelvepain <hexagon643@gmail.com>
2020-11-08 16:52:22 +0700

    Эксперименты с SFML 1

--- SnakeIt/SnakeIt.cpp
+++ SnakeIt/SnakeIt.cpp
@@ -1,7 +1,47 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int WEIGHT = 1280;
+const int HEIGHT = 720;
+
+using namespace sf;
+
+void Run();
+void Draw();
+
+int main(){   
+
+    Run();
+    
+    return 0;
+}
+
+void Run() {
+    Draw();
+}
+
+void Draw() {
+
+    RenderWindow window(VideoMode(WEIGHT, HEIGHT), "SnakeIt");
+
+    while (window.isOpen()){
+
+        Event event;
+        while (window.pollEvent(event)){
+            
+            if (event.type == Event::Closed)
+                window.close();
+
+        }
+
+        window.clear(Color(250, 200, 100, 0));
+
+        RectangleShape menuFone(Vector2f((WEIGHT / 5), (HEIGHT)));
+        menuFone.setFillColor(Color(230, 230, 230, 255));
+        window.draw(menuFone);
+
+
+        window.display();
+
+    }
 }
