22617e4475ff85773e6041b87087c8e2ec72bbe8
Twelvepain <hexagon643@gmail.com>
2020-11-11 12:40:56 +0700

    File reworking: add Tetris and Sapper from SnakeIt.cpp to separate class files(.cpp , .h )

---
+++ SnakeIt/GlobalVars.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _GLOBALVARS_H_
+#define _GLOBALVARS_H_
+
+const int WEIGHT = 1280;
+const int HEIGHT = 720;
+
+bool restart = 0;
+
+#endif // !1
+
+
---
+++ SnakeIt/clTetris.h
@@ -0,0 +1,38 @@
+#pragma once
+class clTetris {
+
+public:
+    void Tetris(int WEIGHT, int HEIGHT, bool *restart);
+private:
+
+    static const int M = 20; // высота игрового поля
+    static const int N = 10; // ширина игрового поля
+
+    //Логическая часть поля
+    int field[M][N] = { 0 };
+
+    int figures[7][4] =
+    {
+        1,3,5,7, // I
+        2,4,5,7, // S
+        3,5,4,6, // Z
+        3,5,4,7, // T
+        2,3,5,7, // L
+        3,5,7,6, // J
+        2,3,4,5, // O
+    };
+
+    //Структура для вывода блоков с вспомогательными массивами
+    struct Point
+    {
+        int x, y;
+    } a[4], b[4];
+
+
+    //Функция проверки проиграна ли игра
+    bool gameOver();
+
+    //Функция проверки выхода за границы поля
+    bool check();
+
+};
