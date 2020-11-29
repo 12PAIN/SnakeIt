a5a836b61650f46a632a4bc9660ab4c1c44e282c
Nikita Dochkin <Dochkin.nik@bk.ru>
2020-11-29 11:58:44 +0700

    Race the game work in progress

--- SnakeIt/clRaceTheGame.cpp
+++ SnakeIt/clRaceTheGame.cpp
@@ -48,7 +48,7 @@ int gameOver()//Функция gameover(Спасибо, кэп)
                 app.close();
         }
         Font myfont;
-        myfont.loadFromFile("Font/arial.ttf");
+        myfont.loadFromFile("Font/xirod.ttf");
         stringscore = "YOUR SCORE:" + to_string(score);
         Text text(stringscore, myfont, 30);
         text.setPosition(210, 450);
@@ -56,6 +56,48 @@ int gameOver()//Функция gameover(Спасибо, кэп)
         app.draw(Gameover);
         app.draw(text);
         app.display();
+
+    }
+
+
+    int main()
+    {
+        //Звук
+        SoundBuffer gameSoundBuffer;
+            gameSoundBuffer.loadFromFile("Audio/game.wav");
+        Sound GameSound;
+            GameSound.setBuffer(gameSoundBuffer);
+
+
+        Texture background, racer, obs1, obs2, obs3, obs4, gameover;
+        Font myfont;
+        myfont.loadFromFile("Font/xirod.ttf");
+
+        //Загрузка текстур
+        if (!background.loadFromFile("Texture/background.png")) return EXIT_FAILURE;
+        if (!racer.loadFromFile("Texture/racer.png")) return EXIT_FAILURE;
+        if (!obs1.loadFromFile("Texture/obs1.png")) return EXIT_FAILURE;
+        if (!obs2.loadFromFile("Texture/obs2.png")) return EXIT_FAILURE;
+        if (!obs3.loadFromFile("Texture/obs3.png")) return EXIT_FAILURE;
+        if (!obs4.loadFromFile("Texture/obs4.png")) return EXIT_FAILURE;
+
+        Sprite Background(background), Background1(background), Racer(racer), Obs1(obs1), Obs2(obs2), Obs3(obs3), Obs4(obs4), Gameover(gameover);
+
+        double RacerX, RacerY, Obs1X, Obs1Y, Obs2X, Obs2Y, Obs3X, Obs3Y, Obs4X, Obs4Y;
+
+        //Позиции машин и гонщика 
+        RacerX = SCREEN_WIDTH / 2;
+        RacerY = SCREEN_HEIGH - racerHeight;
+        Obs1X = getRandomNumber(borderLeft, borderRight);
+        Obs2X = getRandomNumber(borderLeft, borderRight);
+        Obs3X = getRandomNumber(borderLeft, borderRight);
+        Obs4X = getRandomNumber(borderLeft, borderRight);
+        Obs1Y = 0, Obs2Y = -100, Obs3Y = -200, Obs4Y = -300;
+        double BackgroundY1 = 0;
+        double BackgroundY2 = -600;
+
+        GameSound.play();
+        GameSound.setLoop(true);
 
     }
 
