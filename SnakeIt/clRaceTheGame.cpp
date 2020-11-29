e1f226b60e6a861ad9c09a7df940508dbbd173a1
Nikita Dochkin <Dochkin.nik@bk.ru>
2020-11-29 11:36:24 +0700

    Race the game gameover added

--- SnakeIt/clRaceTheGame.cpp
+++ SnakeIt/clRaceTheGame.cpp
@@ -27,3 +27,36 @@ int getRandomNumber(int a, int b)//Функция создает случайное число
     result = (result / 10) * 10;
     return result;
 }
+
+int gameOver()//Функция gameover(Спасибо, кэп)
+{
+    Texture gameover;
+    if (!gameover.loadFromFile("Textures/over.png")) return EXIT_FAILURE;
+
+    Sprite Gameover(gameover);;
+    SoundBuffer gameOver;
+    gameOver.loadFromFile("audio/crash.wav");
+    Sound GameOver;
+    GameOver.setBuffer(gameOver);
+    GameOver.play();
+    while (app.isOpen())
+    {
+        Event event;
+        while (app.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                app.close();
+        }
+        Font myfont;
+        myfont.loadFromFile("Font/arial.ttf");
+        stringscore = "YOUR SCORE:" + to_string(score);
+        Text text(stringscore, myfont, 30);
+        text.setPosition(210, 450);
+        app.clear();
+        app.draw(Gameover);
+        app.draw(text);
+        app.display();
+
+    }
+
+}
